8e07e2cd7739f49fb269b52e00fd2207a92522a2
R. Alex Clark <aclark2089@gmail.com>
2015-12-14 18:19:59 -0500

    Setup main view of app, added UILabels and UITextField to view controller

--- Tooltip/ViewController.swift
+++ Tooltip/ViewController.swift
@@ -9,7 +9,18 @@
 import UIKit
 
 class ViewController: UIViewController {
-
+    
+    // User's Cost Field
+    @IBOutlet weak var costTextField: UITextField!
+    
+    // Main Tip Amount
+    @IBOutlet weak var tipAmountLabel: UILabel!
+    
+    // Split Tip Amounts
+    @IBOutlet weak var split2TipLabel: UILabel!
+    @IBOutlet weak var split3TipLabel: UILabel!
+    @IBOutlet weak var totalLabel: UILabel!
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view, typically from a nib.
